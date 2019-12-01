84e4714b65af1a45ab2b78526dcebf5e65f81fa8
Chris <cnsheafe@pm.me>
2019-11-30 19:55:21 -0700

    init

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+console.log('Hello World from your main file! Update!!!');
+
+const App = <div></div>
+const root = document.getElementById('root')
+ReactDom.render(App, root)
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  context: __dirname,
+  mode: 'development',
+  entry: path.join(__dirname, 'src', 'index'),
+  watch: true,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "bundle.js",
+    chunkFilename: '[name].js'
+  },
+  module: {
+    rules: [{
+      test: /.tsx?$/,
+      include: [
+        path.resolve(__dirname, 'src')
+      ],
+      exclude: [
+        path.resolve(__dirname, 'node_modules')
+      ],
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            presets: [
+              '@babel/preset-react',
+              '@babel/env',
+            ]
+          }
+        },
+        {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          }
+        }
+      ],
+    },
+    ]
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx', '.ts', '.tsx']
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: path.resolve(__dirname, 'dist', 'index.html')
+    }),
+    new ForkTsCheckerWebpackPlugin()
+  ],
+  devtool: 'source-map',
+  devServer: {
+    inline: true,
+    host: 'localhost',
+    port: 3000,
+  }
+};
