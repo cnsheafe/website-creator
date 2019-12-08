d762af0643fbe9321a8f91caa69dd5b10e8dad11
Chris <cnsheafe@pm.me>
2019-12-07 21:38:53 -0700

    Init Editor & Symbol
    todos:
    - Architect markdown parsing
    - Architect symbol architecture
    - Research hidden fields(represent visual text with <div>)

---
+++ src/editor/editor.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+
+type MarkdownTypes = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'img' | 'p' // TODO: Add remaining supported markdown types
+
+interface EditorProps {
+  initialDoc?: string[] // TODO: What data structure should be used to keep track of document lines?
+}
+
+export default function Editor({ initialDoc = [] }: EditorProps) {
+  const [doc] = useState<string[]>(initialDoc)
+  const [focusedLineIndex] = useState<number>(0)
+
+  return (
+    <>
+      {doc.map((line, i) => {
+        if (focusedLineIndex === i) {
+          return <input type="text" value={line} /> // TODO: Support Markdown
+        }
+
+        return <Symbol rawText={line} /> // TODO: Add a click handler to make the new line focused
+      })}
+    </>
+  )
+}
+
+interface SymbolProps { // A symbol can represent markdown or plain text
+  type?: MarkdownTypes
+  rawText: string
+}
+
+export function Symbol({ type, rawText }: SymbolProps) { // TODO: lookup hidden input fields
+  switch (type) {
+    case 'h1':
+      return <h1>{rawText}</h1>// TODO: parse and remove the symbol indicators i.e. `# `
+    default:
+      /** Currently just a span. Might have to change later */
+      return <span>{rawText}</span>
+  }
+}
